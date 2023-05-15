b23aed107a42f7d4f524905e738431e4a89a2ff0
saranyarajakumar <121645385+saranyarajakumar@users.noreply.github.com>
2023-05-15 10:17:56 +0100

    adding Hook file for launching
    adding Hook file for launching

---
+++ Hooks.cs
@@ -0,0 +1,46 @@
+﻿using BoDi;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace Spec_WebTesting.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        
+        private readonly IObjectContainer _container;
+        public Hooks(IObjectContainer container)
+        {
+            _container = container;
+        }
+
+        [BeforeScenario("@AddToCart")]
+        public void BeforeScenarioWithTag()
+        {
+           
+
+        }
+
+        [BeforeScenario]
+         public void FirstBeforeScenario()
+         {
+             IWebDriver driver = new ChromeDriver();
+             driver.Manage().Window.Maximize();
+             driver.Navigate().GoToUrl("https://cms.demo.katalon.com/");
+             _container.RegisterInstanceAs<IWebDriver>(driver);
+         }
+        
+        [AfterScenario]
+            public void AfterScenario()
+        {
+            
+            var driver = _container.Resolve<IWebDriver>();
+            if(driver!=null)
+            {
+               // driver.Quit();
+            }
+        }
+    }
+}
